7b44f2f758ae62c5a6493feb5bd480462fd2042b
JeCkaV <jeckavucko@gmail.com>
2022-05-23 20:54:36 +0200

    Task 3.4.
    Created login and registrations cards, installed axios and propTypes, created forms for log in and registration of the users.

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,9 +1,34 @@
 import React from 'react';
 
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
+
+// Display of movies rendered on the MainView
 export class MovieCard extends React.Component {
   render() {
     const { movie, onMovieClick } = this.props;
-    return <div className="movie-card" onClick={() => { onMovieClick(movie); }}>{movie.Title}</div>;
+    return (
+      <div className="movie-card"onClick={() => {
+          onMovieClick(movie);
+        }}>
+        {movie.Title}
+      </div>
+    );
   }
 }
+
+//setting up default values for the MovieCard properties
+//ensuring values are strings and required
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+      Title: PropTypes.string.isRequired,
+      Description: PropTypes.string.isRequired,
+      Genre: PropTypes.shape({
+      Name: PropTypes.string,
+    }),
+       Director: PropTypes.shape({
+       Name: PropTypes.string,
+    }),
+  }).isRequired,
+
+   onMovieClick: PropTypes.func.isRequired,
+};
