9a21be0d3f280a56575e45ecee321cbb4f0131d0
JeCkaV <jeckavucko@gmail.com>
2022-06-05 09:08:05 +0200

    Codes changed added according to redux

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,29 +1,62 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
-import { Col, Container, Row, Spinner } from "react-bootstrap";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import { LoginView } from "../login-view/login-view";
-import { MovieCard } from "../movie-card/movie-card";
-import { NavbarView } from "../navigationbar-view/navigationbar-view";
+import React from 'react';
+import axios from 'axios';
 
-//getting array of movies from remote and displaying as a list
-export function MainView(props) {
+import { connect } from 'react-redux';
 
-  const [movies, setMovies] = useState([]);
-  const [user, setUser] = useState(props.user);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState();
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-  useEffect(() => {
-    let accessToken = localStorage.getItem('token');
-    if (accessToken !== null) {
-        setUser(localStorage.getItem('user'));
-      }
-    getMovies(accessToken);
-    },[user])
+// #0
+import { setMovies } from '../../actions/actions';
 
-     /* When a user successfully logs in, this function updates the `user` property in state to that *particular user*/
-     function onLoggedIn(authData) {
+// we haven't written this one yet
+import MoviesList from '../movies-list/movies-list';
+/* 
+  #1 The rest of components import statements but without the MovieCard's 
+  because it will be imported and used in the MoviesList component rather
+  than in here. 
+*/
+
+
+// #2 export keyword removed from here
+class MainView extends React.Component {
+
+  constructor() {
+    super();
+
+    // #3 movies state removed from here
+    this.state = {
+      user: null
+    };
+  }
+  //This will be executed after loading the page
+  componentDidMount() 
+  { axios
+    .get('https://radiant-depths-97196.herokuapp.com/movies')
+    .then(response => {
+        this.setState({ movies: response.data });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  //fetch movies from API
+  getMovies(token) {
+       const response =  await axios.get('https://radiant-depths-97196.herokuapp.com/movies', {
+       headers: { Authorization: `Bearer ${token}`}
+      })
+      .then(response => {
+
+        // #4
+        this.props.setMovies(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+    }
+
+    /* When a user successfully logs in, this function updates the `user` property in state to that *particular user*/
+    onLoggedIn(authData) {
       console.log("OnLoggedIn...");
       setUser(authData.user.Username);
       localStorage.setItem('token', authData.token);
@@ -32,59 +65,34 @@ export function MainView(props) {
 
     }
 
-  //fetch movies from API
-  async function getMovies(token) {
-       const response =  await axios.get('https://radiant-depths-97196.herokuapp.com/movies', {
-       headers: { Authorization: `Bearer ${token}`},
-      });
-      setMovies(response.data)
-      setLoading(false);
+    render() {
+
+      // #5 movies is extracted from this.props rather than from the this.state
+      let { movies } = this.props;
+      let { user } = this.state;
+  
+      return (
+        <Router>
+          <Row className="main-view justify-content-md-center">
+            <Route exact path="/" render={() => {
+              if (!user) return <Col>
+                <LoginView onLoggedIn={user => this.onLoggedIn(user)} />
+              </Col>
+              if (movies.length === 0) return <div className="main-view" />;
+              // #6
+              return <MoviesList movies={movies}/>;
+            }} />
+            {/* The rest of routes */}
+          </Row>
+        </Router>
+            );
     }
-
- //If data is not fetched, show spinner
-  if ((user) && (loading)) {
-    return (
-          <Row className="justify-content-center my-5">
-             <div className="h3 text-muted text-center"> Loading Movies...&nbsp; 
-             <Spinner animation="border" variant="secondary" role="status" />
-          </div>
-        </Row>
-    );		
-   }
-
-  if (error) {
-    return (
-       <Row className="justify-content-center my-5">
-      <p>There was an error loading your data!</p>
-      </Row>
-      );
   }
-
-  return (
-    <>
-      <Router>
-        <NavbarView />
-        <Container>
-          <Route
-            path='/'
-            render={() => {
-              if (!user)
-                return (
-                  <Col>
-                    <LoginView onLoggedIn={(user) => this.onLoggedIn(user)} />
-                  </Col>
-                );
-              if (movies.length === 0) return <div className='main-view'></div>;
-              return movies.map((m) => (
-                <Col md={3} key={m._id}>
-                  <MovieCard movie={m} />
-                </Col>
-              ));
-            }}
-          />
-        </Container>
-      </Router>
-    </>
-  );
-}
-
+  
+  // #7
+  let mapStateToProps = state => {
+    return { movies: state.movies }
+  }
+  
+  // #8
+  export default connect(mapStateToProps, { setMovies } )(MainView);
--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,11 @@
-import React, {useState, useEffect} from 'react';
-import { createRoot } from 'react-dom/client';
-import { MainView } from './components/main-view/main-view';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import Container from 'react-bootstrap/Container';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import moviesApp from './reducers/reducers';
+
+import MainView from './components/main-view/main-view';
 
 // Import statement to indicate that you need to bundle `./index.scss`
 import './index.scss';
@@ -27,9 +28,6 @@ class MyFlixApplication extends React.Component {
 
 // Finds the root of your app
 const container = document.getElementsByClassName('app-container')[0];
-const root = createRoot(container); 
-// Tells React to render your app in the root DOM element
-root.render(<MyFlixApplication tab="home" />);
 
 //Tells React to render app in root of DOM element
 ReactDOM.render(React.createElement(MyFlixApplication), container);
